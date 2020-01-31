4a400cbbecd4cfc9c14f62d6ce6cf0b1184cd37a
Mario Vega <mario@ark.io>
2020-01-30 19:04:46 -0800

    commit argon-car frontend before backup

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,164 +1,114 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript"
-          target="_blank"
-          rel="noopener"
-          >typescript</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa"
-          target="_blank"
-          rel="noopener"
-          >pwa</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router"
-          target="_blank"
-          rel="noopener"
-          >router</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex"
-          target="_blank"
-          rel="noopener"
-          >vuex</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-unit-jest"
-          target="_blank"
-          rel="noopener"
-          >unit-jest</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-e2e-cypress"
-          target="_blank"
-          rel="noopener"
-          >e2e-cypress</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
-  </div>
+  <main class="flex-center-column">
+    <h1>Welcome to my app</h1>
+
+    <section class="video">
+      <span>Main Video Component</span>
+      <span>Current queue: {{ commandQueue.join(" ") }}</span>
+      <span>Previous queues: {{ previousCommandQueues.length }}</span>
+
+      <ol
+        v-for="(prevQueue, index) in previousCommandQueues"
+        :key="index"
+        class="video"
+      >
+        <li v-for="(cmd, cmdIndex) in prevQueue" :key="cmdIndex">
+          {{ cmd }}
+        </li>
+      </ol>
+    </section>
+    <section class="controls-wrapper">
+      <div>Left</div>
+      <div>Up/Down</div>
+      <div>Right</div>
+    </section>
+  </main>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
+import Vue from "vue"
+import { throttle } from "lodash"
+
+interface RCData {
+  commandMap: { [s: string]: string }
+  commandQueue: CommandQueue
+  previousCommandQueues: CommandQueue[]
+}
+
+type CommandQueue = string[]
 
 export default Vue.extend({
-  name: "HelloWorld",
-  props: {
-    msg: String
+  data(): RCData {
+    return {
+      commandMap: {
+        ArrowUp: "F",
+        ArrowDown: "B",
+        ArrowLeft: "L",
+        ArrowRight: "R"
+      },
+      commandQueue: [],
+      previousCommandQueues: []
+    }
+  },
+
+  mounted() {
+    window.addEventListener("keydown", this.processKeydown)
+  },
+
+  updated() {
+    if (this.commandQueue.length > 0) {
+      this.processQueue()
+    }
+  },
+
+  destroyed() {
+    window.removeEventListener("keydown", this.processKeydown)
+  },
+
+  methods: {
+    processKeydown(event: KeyboardEvent): void {
+      event.preventDefault()
+
+      if (Object.keys(this.commandMap).includes(event.key)) {
+        this.addCommand(this.commandMap[event.key])
+      }
+    },
+
+    addCommand(command: string): void {
+      this.commandQueue = [...this.commandQueue, command]
+    },
+
+    processQueue: throttle(function(): void {
+      this._processQueue()
+    }, 1000),
+
+    _processQueue(): void {
+      this.previousCommandQueues.push(this.commandQueue)
+      this.commandQueue = []
+    }
   }
-});
+})
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.flex-center-column {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.controls-wrapper {
+  display: flex;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.controls-column {
+  display: flex;
+  flex-direction: column;
 }
-a {
-  color: #42b983;
+
+.video {
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 10vh;
 }
 </style>
